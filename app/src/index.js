c5fb8ef0f2840bda10ef9dd2d688bf1b91a94485
gerardfp <gerardfp@gmail.com>
2021-09-14 22:25:25 +0200

    .

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./mysql');
+const uuid = require('uuid/v4');
 
 app.use(require('body-parser').json());
 app.use(express.static(__dirname + '/static'));
